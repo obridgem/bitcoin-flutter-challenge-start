42aed52f900eb88c0b00a58caa40ca22f1693bd3
obridgem <olivier@bridgeweb.dev>
2019-09-24 12:49:06 -0400

    Completed project.

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,4 +1,6 @@
 //TODO: Add your imports here.
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 const List<String> currenciesList = [
   'AUD',
@@ -35,4 +37,18 @@ const bitcoinAverageURL =
 
 class CoinData {
   //TODO: Create your getCoinData() method here.
+  Future<double> getCoinData() async {
+    double coinPrice = 0;
+    http.Response coinInfoResponse =
+        await http.get(bitcoinAverageURL + '/BTCUSD');
+    if (coinInfoResponse.statusCode == 200) {
+      var coinInfo = jsonDecode(coinInfoResponse.body);
+      coinPrice = coinInfo['last'];
+    } else {
+      print('coinInfoResponse.statusCode: ');
+      print(coinInfoResponse.statusCode);
+      throw 'Error in getCoinData()';
+    }
+    return coinPrice;
+  }
 }
